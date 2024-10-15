ddb3443bcbcf3633b509f96c3ac8e2e0d4922b2a
84gE0VwEL6S6RxC8G84QwBr4vvCvmUV3CL8VeMoOCZ7woa5JYGmiCDHHJwxewJJAvd2ZTe3sYn6JAcfEs4 <vigilante@VigiLante.VigiLante>
2024-10-15 13:25:38 -0400

    Update structure (init.sql) 'devops'

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,15 @@
-CREATE TABLE emails (
+CREATE USER slave_user REPLICATION LOGIN PASSWORD '${DB_REPL_PASSWORD}';
+
+CREATE DATABASE ${DB_DATABASE};
+
+\c db_data;
+
+CREATE TABLE IF NOT EXISTS  emails (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE phone_numbers (
+CREATE TABLE IF NOT EXISTS phone_numbers (
     id SERIAL PRIMARY KEY,
     phone_number VARCHAR(15) NOT NULL
 );
